1ff1570a5222594cc9c0bcbca14f28ee821a8e27
David Jungst <djungst@gmail.com>
2017-11-13 10:48:08 -0700

    start high low game

--- high_low.rb
+++ high_low.rb
@@ -4,10 +4,26 @@ class HighLow
   def initialize(player, casino)
     @player = player
     @casino = casino
-    puts "Do you want to play High Low"
-    choice = gets.strip
-    if choice.downcase == 'no'
+    menu
+  end
+
+  def menu
+    puts "1) Place Bet"
+    puts "2) Return to Casino"
+    choice = gets.to_i
+    if choice == 1
+      place_bet
+    elsif choice == 2
       @casino.menu
+    else
+      puts "Invalid choice.  Try again"
+      menu
     end
+  end
+
+  def place_bet
+    puts "How much do you want to bet? ($#{@player.wallet})"
+    amount = gets.to_f
+    #TODO Flip cards
   end
 end
